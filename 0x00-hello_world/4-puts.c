fbd488e8c9979d69069e5b6ab6f28b9c501572bb
Bereket Seifu <beki7m2@gmail.com>
2023-03-16 22:41:50 +0000

    int main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /*
- * print using puts
+ * main - no argument
+ * print the statment using puts
+ *
+ * return: zero
  */
 
- main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - no argument
+ * print the statment
+ *
+ * Return: zero
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
