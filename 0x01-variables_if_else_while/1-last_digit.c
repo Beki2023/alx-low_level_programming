756838a920549ebf82b0a23d56b383d1fb8d5449
Bereket Seifu <beki7m2@gmail.com>
2023-03-17 20:11:32 +0000

    mixd declaretion

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-	int n;
+	int n, i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int i = n % 10;
+	i = n % 10;
 
 	if (i > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, i);
