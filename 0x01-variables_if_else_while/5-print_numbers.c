a8855518ef1b1b81b88695c4a71e24aee58b598c
Bereket Seifu <beki7m2@gmail.com>
2023-03-18 10:06:13 +0000

    ten base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - no argument
+ * print the the all lowercase exept e and q
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	return (0);
+}
