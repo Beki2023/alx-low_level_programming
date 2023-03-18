3a27f5d221c2d21efa42c3d4cd6c1ac4e435d8c7
Bereket Seifu <beki7m2@gmail.com>
2023-03-18 10:23:27 +0000

    betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,16 @@
 
 /**
  * main - no argument
- * print the the all lowercase exept e and q
+ * print all numbers with base ten 
  *
  * Return: zero
  */
 
 int main(void)
- {
-        int i;
-        for (i = '0'; i <= '9'; i++)
-            putchar(i);
-    return (0);
-    }
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	return (0);
+}
