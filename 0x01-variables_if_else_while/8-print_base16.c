c4aa406f2fcf1855f7bdacd2a726cb5042134ea5
Bereket Seifu <beki7m2@gmail.com>
2023-03-19 07:51:56 +0000

    base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * base 16 in lowercase
+ *
+ * return: zero
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	return (0);
+}
