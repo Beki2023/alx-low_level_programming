f421a8e5f2070d40f9926f5d30c910788c77fc11
Bereket Seifu <beki7m2@gmail.com>
2023-03-21 01:06:11 +0000

    two combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 1; j <= 9; j++)
+	{
+		if (i < j)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+}
+putchar('\n');
+return (0);
+}
