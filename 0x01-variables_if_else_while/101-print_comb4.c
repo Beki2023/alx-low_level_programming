75fc255e715e26610fffcb57c7387fa91b86f1de
Bereket Seifu <beki7m2@gmail.com>
2023-03-21 02:08:58 +0000

    spaling

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,23 +10,23 @@ int i, j, k;
 
 for (i = 0; i < 9; i++)
 {
-for (j = 1; j <= 9; j++)
-{
-for (k = 2; k <= 9; k++)
-{
-	if (i < j && j < k)
+	for (j = 1; j < 9; j++)
 	{
-		putchar(i + '0');
-		putchar(j + '0');
-		putchar(k = '0');
-		if ((i + j + k) != 24)
+		for (k = 2; k < 10; k++)
 		{
-		putchar(',');
-		putchar(' ');
+			if (i < j && j < k)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if ((i + j + k) != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-}
-}
 }
 putchar('\n');
 return (0);
