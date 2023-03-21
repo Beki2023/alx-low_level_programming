1066cb799c0f2c455be8eb2de31ff0d9f0312dd3
Bereket Seifu <beki7m2@gmail.com>
2023-03-21 00:03:33 +0000

    singile digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always zero
+ */
 
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = "0, "; i <= "9 "; i++)
+	for (i = 0; i <= 9; i++)
+		if (i == 9)
+		putchar(i + '0');
+		else
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		}
+	return (0);
+}
