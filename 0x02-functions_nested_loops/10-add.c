d87ef8034ae77e54ee73967574450807e696e959
Bereket Seifu <beki7m2@gmail.com>
2023-03-23 00:41:01 +0000

    7&8

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+ * add - adding two number
+ * @i: the number to be added
+ * @j: the second number
+ *
+ * Return: the rusult of the added value
+ */
+int add(int i, int j)
+{
+	int result = i + j;
+
+	return (result);
+}
